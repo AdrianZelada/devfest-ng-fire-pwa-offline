dc18a796b8b7544e23ce8aed5426b3433d5868d2
adrian zelada torrez <zelada_torrez@hotmail.com>
2019-11-07 00:52:32 -0400

    category routes

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './category.component';
+import {CategoryRoutingModule} from "./category-routing.module";
 
 
 
 @NgModule({
   declarations: [CategoryComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CategoryRoutingModule
   ]
 })
 export class CategoryModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    appId: "xxxxxxxxxxxxxxxxxxxxxxxx"
+    apiKey: "AIzaSyBRPlquR-qhlbOk_htuC64gpiAf0jkmy0U",
+    authDomain: "scorching-fire-4444.firebaseapp.com",
+    databaseURL: "https://scorching-fire-4444.firebaseio.com",
+    projectId: "scorching-fire-4444",
+    storageBucket: "scorching-fire-4444.appspot.com",
+    messagingSenderId: "378160403959",
+    appId: "1:378160403959:web:42c8db91c7fc6c48210af2"
   }
 };
 
